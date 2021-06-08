ec37993003b35e16c472aa9a97203646dfc3d477
Anna Timofeeva <anna@timofeeva.pw>
2021-06-08 15:21:58 +0100

    Added more if-else statements

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# Assign a numeric value
+seconds = 29
+
+# Adding another print statement
+print("Possibly unnecessary")
+
+# Check if the number of seconds is less than or equal to 60 (one minute)
+if (seconds < 61):
+    print("Under one minute")
+    print("This is Phyton babe")
+
+    if (seconds<30):
+        print("It is less than half a minute")
+else:
+    print("More than one minute")
