a70e887b49b40eab4da1244daeb8675f712efd75
Anna Timofeeva <anna@timofeeva.pw>
2021-06-09 12:33:52 +0100

    added 1 more file  - homework added

---
+++ Homework1.py
@@ -0,0 +1,41 @@
+# PascalCaseString = "PascalCaseString"
+# camelCaseString = "camelCaseString"
+
+# Pascal_To_Camel_String = PascalCaseString[0].lower()+PascalCaseString[1:]
+# Camel_To_Pascal_String = camelCaseString[0].upper()+camelCaseString[1:]
+
+# print("Pascal to camel string: "+Pascal_To_Camel_String)
+# print("Camel to Pascal string: "+Camel_To_Pascal_String)
+
+
+# ibanDecoder
+#     A Irish IBAN consists of 22 alpha-numeric characters:
+#      - 2 letter country code
+#      - 2 digit checksum number
+#      - 4 characters from the bank's SWIFT/BIC or bank identification number
+#      - 6 digit code for the bank branch
+#      - 8 digit code for the bank account number
+     
+#     Write some code that takes a 22 character IBAN number and extracts 
+#     the 5 pieces of information from IBAN (country, checksum, BIC, branch, a/c num)
+
+def camel2Pascal(camelText):
+
+    pascalText = camelText[0].upper()+camelText[1:]
+    return pascalText
+
+name_camel = '1TwoThree'
+name_pascal = camel2Pascal(name_camel)
+print("Camel case = "+name_camel)
+print("Pascal case = "+name_pascal)
+
+
+# IBANString = "IE12345678901234567890"
+
+# country = IBANString[0:2]
+# checksum = IBANString[2:4]
+# BIC = IBANString[4:8]
+# Branch = IBANString[8:14]
+# AccountNumber = IBANString[14:]
+
+# print("Country: "+country+" checksum: "+checksum+ " BIC: "+BIC+ " branch: "+Branch+ " Account number "+AccountNumber)
